64e18825e0c5b04726a3b24e71017e1f55c5688c
Vlad Aristakesyan <arut.73@gmail.com>
2019-10-28 22:04:35 +0300

    calculating result table with id`s table and id`s constructions

--- compiler.js
+++ compiler.js
@@ -2,6 +2,10 @@ const fs = require('fs')
 const path = require('path')
 
 const tokensPath = path.join(__dirname, 'tokens.json')
+const tablePath = path.join(__dirname, 'table.json')
+const identPath = path.join(__dirname, 'ident.json')
+const literalsPath = path.join(__dirname, 'literals.json')
+const resultPath = path.join(__dirname, 'result.json')
 
 const lexer = code => {
   const { identifiers, delimiters } = JSON.parse(fs.readFileSync(tokensPath, 'utf-8'))
@@ -25,7 +29,8 @@ const lexer = code => {
       if (identifiers.includes(construction)) {
         table.push({
           value: construction,
-          text: 'Идентификатор'
+          text: 'Идентификатор',
+          key: 'ident'
         })
 
         continue
@@ -34,7 +39,8 @@ const lexer = code => {
       if (delimiters.includes(construction)) {
         table.push({
           value: construction,
-          text: 'Разделитель'
+          text: 'Разделитель',
+          key: 'del'
         })
 
         continue
@@ -53,12 +59,14 @@ const lexer = code => {
             if (delimiters.includes(symbol)) {
               table.push({
                 value: symbol,
-                text: 'Разделитель'
+                text: 'Разделитель',
+                key: 'del'
               })
             } else {
               table.push({
                 value: symbol,
-                text: 'Литерал'
+                text: 'Литерал',
+                key: 'lit'
               })
             }
           })
@@ -67,18 +75,54 @@ const lexer = code => {
       if (isLiteral) {
         table.push({
           value: construction,
-          text: 'Литерал'
+          text: 'Литерал',
+          key: 'lit'
         })
       }
     }
   }
 
-  return table
+  table = table
     .filter(item => item.value)
     .map(item => {
       item.value = item.value.replace('\r', '')
       return item
     })
+
+  const lits = [...new Set(table.filter(item => item.key === 'lit').map(item => item.value))]
+
+  const codeLiterals = lits.filter(Number)
+  const codeIdents = lits.filter(item => !codeLiterals.includes(item))
+
+  const result = table
+    .map(item => {
+      if (item.key === 'ident') {
+        return { table: 1, position: identifiers.findIndex(identifier => identifier === item.value) }
+      }
+
+      if (item.key === 'del') {
+        return { table: 2, position: delimiters.findIndex(delimiter => delimiter === item.value) }
+      }
+
+      if (item.key === 'lit') {
+        const literalId = codeLiterals.findIndex(lit => lit === item.value)
+        const identId = codeIdents.findIndex(idt => idt === item.value)
+
+        if (literalId > -1) {
+          return { table: 3, position: literalId }
+        }
+
+        if (identId > -1) {
+          return { table: 4, position: identId }
+        }
+      }
+    })
+    .filter(Boolean)
+
+  fs.writeFileSync(tablePath, JSON.stringify(table))
+  fs.writeFileSync(identPath, JSON.stringify(codeIdents))
+  fs.writeFileSync(literalsPath, JSON.stringify(codeLiterals))
+  fs.writeFileSync(resultPath, JSON.stringify(result))
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const path = require('path')
 const compiler = require('./compiler')
 
 const codePath = path.join(__dirname, 'code.txt')
-const tablePath = path.join(__dirname, 'table.json')
 
 const code = fs.readFileSync(codePath, 'utf-8')
 
-const table = compiler.lexer(code)
-
-fs.writeFileSync(tablePath, JSON.stringify(table))
+compiler.lexer(code)
